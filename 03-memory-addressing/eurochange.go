af4ccd1705954feb19a71e33427b9577a4490fa0
Jordan Chong <jchong011@gmail.com>
2017-08-21 10:27:31 -0600

    commented my eurochange program

--- 03-memory-addressing/eurochange.go
+++ 03-memory-addressing/eurochange.go
@@ -5,10 +5,10 @@ import "fmt"
 const euroconv float64 = .85 //this is the current euro>usd converstion rate
 
 func main() {
-	var usd float64
+	var usd float64 //expect the variable usd to be a float64 value
 	fmt.Print("How Many USD Dollars Do You Have?")
-	fmt.Scan(&usd)
-	euros := usd * euroconv
+	fmt.Scan(&usd) //fmt.Scan will parse the returned input and the & stores the returned value in memory
+	euros := usd * euroconv //just assigning a var to a formula
 	fmt.Println(usd, "Dollars is currently", euros, "Euros")
 
 }
